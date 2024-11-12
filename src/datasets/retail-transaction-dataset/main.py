e036dcbe2963b176e64d6d71900f6311ae1c8aef
Vraj Patel <vrajp3301@gmail.com>
2024-11-11 19:18:34 -0500

    Update main.py
    Update main,py based on the feedback received on discord

--- src/datasets/retail-transaction-dataset/main.py
+++ src/datasets/retail-transaction-dataset/main.py
@@ -7,39 +7,38 @@ def csv_to_jsonl(csv_file_path, jsonl_file_path, dataset_link, dataset_name):
         
         with jsonlines.open(jsonl_file_path, mode='w') as jsonl_file:
             for id, row in enumerate(csv_reader):
+                text = (
+                    f"Customer_ID: {row['Customer_ID']}\n"
+                    f"Name: {row['Name']}\n"
+                    f"Email: {row['Email']}\n"
+                    f"Phone: {row['Phone']}\n"
+                    f"Address: {row['Address']}\n"
+                    f"City: {row['City']}\n"
+                    f"State: {row['State']}\n"
+                    f"Zipcode: {row['Zipcode']}\n"
+                    f"Country: {row['Country']}\n"
+                    f"Age: {row['Age']}\n"
+                    f"Gender: {row['Gender']}\n"
+                    f"Income: {row['Income']}\n"
+                    f"Customer_Segment: {row['Customer_Segment']}\n\n"
+                    f"Date: {row['Date']}\n"
+                    f"Year: {row['Year']}\n"
+                    f"Month: {row['Month']}\n"
+                    f"Product_Category: {row['Product_Category']}\n"
+                    f"Product_Brand: {row['Product_Brand']}\n"
+                    f"Product_Type: {row['Product_Type']}\n"
+                    f"Feedback: {row['Feedback']}\n"
+                    f"Shipping_Method: {row['Shipping_Method']}\n"
+                    f"Payment_Method: {row['Payment_Method']}\n"
+                    f"products: {row['products']}"
+                )   
                 json_line = {
                     'meta': {
                         'dataset_link': dataset_link,
                         'dataset_name': dataset_name,
                         'id': id
                     },
-                    'customer_info': {
-                        'Customer_ID': row['Customer_ID'],
-                        'Name': row['Name'],
-                        'Email': row['Email'],
-                        'Phone': row['Phone'],
-                        'Address': row['Address'],
-                        'City': row['City'],
-                        'State': row['State'],
-                        'Zipcode': row['Zipcode'],
-                        'Country': row['Country'],
-                        'Age': row['Age'],
-                        'Gender': row['Gender'],
-                        'Income': row['Income'],
-                        'Customer_Segment': row['Customer_Segment'],
-                    },
-                    'purchase_info': {
-                        'Date': row['Date'],
-                        'Year': row['Year'],
-                        'Month': row['Month'],
-                        'Product_Category': row['Product_Category'],
-                        'Product_Brand': row['Product_Brand'],
-                        'Product_Type': row['Product_Type'],
-                        'Feedback': row['Feedback'],
-                        'Shipping_Method': row['Shipping_Method'],
-                        'Payment_Method': row['Payment_Method'],
-                        'products': row['products']
-                    }
+                    'text': text
                 }
                 jsonl_file.write(json_line)
 
